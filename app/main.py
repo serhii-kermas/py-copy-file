7244029d71f590261c40c1c0d8362b38a20abb42
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-28 13:54:27 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    command_kv = command.split()
+
+    if (len(command_kv) == 3 and command_kv[0] == "cp"
+            and command_kv[1] != command_kv[2]
+            and os.path.exists(command_kv[1])):
+        with (open(command_kv[1], "r") as file_in,
+              open(command_kv[2], "w") as file_out):
+            file_out.write(file_in.read())
